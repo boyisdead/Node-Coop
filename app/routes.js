37862a1ef71790731131ee0788de56b61bd0c7b4
boyisdead <nattawut_k@cmu.ac.th>
2015-11-06 14:33:45 +0700

    [nCoop]-004
     - student update and document update was added

--- app/routes.js
+++ app/routes.js
@@ -12,7 +12,7 @@ function studentLogin(item, res, app) {
     console.log("find Student with : " + item.username);
     Student.findOne({
         "stu_code": item.username
-    }, function(err, student) {
+    }, function (err, student) {
         if (err)
             res.send(err);
         if (!student) {
@@ -57,7 +57,7 @@ function teacherLogin(item, res, app) {
     console.log("find Teacher with : " + item.username);
     Teacher.findOne({
         "staff_code": item.username
-    }, function(err, teacher) {
+    }, function (err, teacher) {
         if (err)
             res.send(err);
         if (!teacher) {
@@ -102,7 +102,7 @@ function teacherLogin(item, res, app) {
 
 function getStudents(res) {
     console.log("get student list");
-    Student.find(function(err, students) {
+    Student.find(function (err, students) {
 
         // if there is an error retrieving, send the error. nothing after res.send(err) will execute
         if (err)
@@ -114,7 +114,7 @@ function getStudents(res) {
 
 function findStudentByCode(item, res) {
     console.log("Find student with " + item);
-    Teacher.findOne({'stu_code':item},function(err, students) {
+    Teacher.findOne({'stu_code':item},function (err, students) {
 
         // if there is an error retrieving, send the error. nothing after res.send(err) will execute
         if (err)
@@ -136,7 +136,7 @@ function createStudent(item, res) {
         password: "1234" // random pass algo here 
     });
 
-    newStudent.save(function(err) {
+    newStudent.save(function (err) {
         if (err){
             res.send(err);
         }
@@ -194,7 +194,7 @@ function delStudent(item, res) {
 //============================ Teacher function API ==============================
 
 function getTeacher(res) {
-    Teacher.find(function(err, teachers) {
+    Teacher.find(function (err, teachers) {
 
         // if there is an error retrieving, send the error. nothing after res.send(err) will execute
         if (err)
@@ -232,6 +232,24 @@ function createTeacher(item, res) {
         getTeacher(res);
     })
 };
+
+function updateTeacher(item, res){
+    Teacher.findOne({ _id: item._id }, function (err, doc){
+        if(doc!=null){
+            doc.sex = item.sex;
+            doc.tel = item.tel;
+            doc.contact_email = item.contact_email;
+            doc.name_th = item.name_th;
+            doc.name_en = item.name_en;
+            doc.password = item.password;
+            doc.save();
+        } else console.log("Not found - not update");
+
+        if(err)
+            res.send(err);
+        getTeacher(res);     
+    });
+}
 
 function delTeacher(item, res) {
     Teacher.remove({
@@ -290,6 +308,19 @@ function createDocument(item, res){
             
 }
 
+function updateDocument(item, res){
+    Document.findOne({ _id: item._id }, function (err, doc){
+        doc.status = item.status;
+        doc.comment = item.comment;
+        doc.save();
+
+        if(err)
+            res.send(err);
+        else
+            getDocument(res);
+    });
+}
+
 function delDocument(item, res) {
     Document.remove({
         _id: item
@@ -306,7 +337,7 @@ function delDocument(item, res) {
 module.exports = function(app) {
 
     // authenticate to obtains token
-    app.post('/api/auth', function(req, res) {
+    app.post('/api/auth', function (req, res) {
         console.log("Hello login");
         if (req.body.type == "student"){
             console.log("Student login");
@@ -357,34 +388,34 @@ module.exports = function(app) {
     
     // -----------------------------------Student routes ----------------------------------------------------
     // get all students
-    app.get('/api/students', function(req, res) {
+    app.get('/api/students', function (req, res) {
         getStudents(res);
     });
 
-    app.get('/api/students/:staff_code', function(req, res) {
+    app.get('/api/students/:staff_code', function (req, res) {
         findStudentByCode(req.params.stu_code, res);
     });
 
     // create student and send back all students after creation
-    app.post('/api/students', function(req, res) {
+    app.post('/api/students', function (req, res) {
         console.log("Creating...");
         createStudent(req.body, res);
     });
 
     // update a student
-    app.put('/api/students', function(req, res){
+    app.put('/api/students', function (req, res){
         console.log("Updating...");
         updateStudent(req.body, res);
     });
 
     // delete a student
-    app.delete('/api/students/:student_id', function(req, res) {
+    app.delete('/api/students/:student_id', function (req, res) {
         delStudent(req.params.student_id, res);
     });
 
     // ---------------------------------- Teacher routes ------------------------------------------------------
     // 
-    app.get('/api/teachers', function(req, res) {
+    app.get('/api/teachers', function (req, res) {
         getTeacher(res);
     });
 
@@ -393,18 +424,22 @@ module.exports = function(app) {
     });
 
     // create teacher and send back all teachers after creation
-    app.post('/api/teachers', function(req, res) {
+    app.post('/api/teachers', function (req, res) {
         createTeacher(req.body, res);
     });
+    // update a teacher
+    app.put('/api/teachers', function (req, res) {
+        updateTeacher(req.body, res);
+    })
 
     // delete a teacher
-    app.delete('/api/teachers/:teacher_id', function(req, res) {
+    app.delete('/api/teachers/:teacher_id', function (req, res) {
         delTeacher(req.params.teacher_id, res);
     });
 
     // -----------------------------------Document routes ----------------------------------------------------
     // get all documents
-    app.get('/api/documents', function(req, res) {
+    app.get('/api/documents', function (req, res) {
         getDocument(res);
     });
 
@@ -413,15 +448,20 @@ module.exports = function(app) {
         createDocument(req, res, next);
     });
 
+    // update document
+    app.put('/api/documents', function (req, res) {
+        updateDocument(req.body,res);
+    })
+
     // delete a document
-    app.delete('/api/documents/:document_id', function(req, res) {
+    app.delete('/api/documents/:document_id', function (req, res) {
         delDocument(req.params.document_id, res);
     });
 
 
 
     // application -------------------------------------------------------------
-    app.get('*', function(req, res) {
+    app.get('*', function (req, res) {
         res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
     });
 };
