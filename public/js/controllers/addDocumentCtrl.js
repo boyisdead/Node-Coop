ee2fe4345686178d3f5b9b62bf995536ff50162f
boyisdead <nattawut_k@cmu.ac.th>
2015-11-20 17:57:58 +0700

    [nCoop]-010
     - validate add doc form

--- public/js/controllers/addDocumentCtrl.js
+++ public/js/controllers/addDocumentCtrl.js
@@ -27,8 +27,43 @@ documentModule.controller('addDocumentCtrl', ['$scope','Upload', '$modalInstance
         }
     };
 
+    $scope.validateForm = function (msg){
+        var errList = "";
+        if (typeof $scope.formData != 'undefined') {
+            if (msg.scode.$error.required) {
+                errList += "รหัสนักศึกษา ไม่ถูกกรอก\n";
+            } else if (msg.scode.$error.minlength) {
+                errList += "รหัสนักศึกษา สั้นเกินไป\n";
+            }
+
+            if (msg.file.$error.required) {
+                errList += "ไม่มีเอกสารที่เลือก\n";
+            }
+
+            if (!msg.file.$valid) {
+                errList += "เอกสารไม่ถูกต้อง\n";
+            }
+
+            if (msg.fileType.$error.required) {
+                errList += "ประเภทเอกสารไม่ถูกเลือก ไม่ถูกเลือก\n";
+            }
+
+            if (msg.desc.$error.required) {
+                if($scope.formData.file_type=="Certificate")
+                    errList += "ใบรับรองต้องมีคำอธิบาย\n";
+            }
+
+            if (errList != "") {
+                sweetAlert("ฟอร์มไม่ถูกต้อง!", errList, 'error');
+            } else {
+                console.log("aaa");
+                upload($scope.formData.attachFile);
+            }
+        }
+    }
+
     // upload on file select or drop
-    $scope.upload = function (file) {
+    var upload = function (file) {
         console.log(file);
         Upload.upload({
             url: '/api/documents/upload',
@@ -37,12 +72,20 @@ documentModule.controller('addDocumentCtrl', ['$scope','Upload', '$modalInstance
             file: file
         }).then(function (resp) {
             console.log('Success' , resp.config, resp.success);
+            swal({
+                title:"สำเร็จ",
+                type:"success",
+                text:"เอกสารได้ถูกเพิ่มแล้ว",
+                confirmButtonText:"ปิด",
+            });
+            $modalInstance.close();
             //console.log('Success ' + resp.config.attachFile.name + 'uploaded. Response: ' , resp.data);
         }, function (resp) {
             console.log('Error status: ' + resp.status);
         }, function (evt) {
             var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
             console.log('progress: ' , progressPercentage);
+
             //console.log('progress: ' + progressPercentage + '% ' + evt.config.attachFile.name);
         });
     };
